4c145879c7b3998bcc9df9d85e9010882352ece9
infinitbility <infinitbility@gmail.com>
2021-11-14 17:00:43 +0530

    React Navigation

---
+++ src/navigation/StackNavigator.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Image } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "../screens/Home";
+import About from "../screens/About";
+import Contact from "../screens/Contact";
+import Splash from "../screens/Splash";
+
+import BottomTabNavigator from "./TabNavigator";
+
+const Stack = createStackNavigator();
+
+const screenOptionStyle = {
+  headerStyle: {
+    backgroundColor: "#9AC4F8",
+  },
+  headerTintColor: "white",
+  headerBackTitle: "Back",
+};
+
+const HomeStackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+      <Stack.Screen name="Home" component={Home} options={{headerLeft: (props) => null }}  />
+      <Stack.Screen name="About" component={About} />
+    </Stack.Navigator>
+  );
+}
+
+const ContactStackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+      <Stack.Screen name="Contact" component={Contact} options={{ headerLeft: (props) => null }} />
+    </Stack.Navigator>
+  );
+}
+
+export default MainStackNavigator = () => {
+
+    return (
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Splash" component={Splash} />
+          <Stack.Screen name="TabNavigator" component={BottomTabNavigator} />
+        </Stack.Navigator>
+    );
+}
+
+export { HomeStackNavigator, ContactStackNavigator };
---
+++ src/navigation/TabNavigator.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Image } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { HomeStackNavigator, ContactStackNavigator } from "./StackNavigator";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+    return (
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+            <Tab.Screen
+                name="Home"
+                component={HomeStackNavigator}
+                options={{
+                    tabBarIcon: () => (<Image source={require("./../assets/icons/home-outline-svg.png")} style={{width: 20, height: 20}} />)
+                }}
+                 />
+            <Tab.Screen 
+                name="Contact" 
+                component={ContactStackNavigator}
+                options={{
+                    tabBarIcon: () => (<Image source={require("./../assets/icons/people-outline-svg.png")} style={{width: 20, height: 20}} />)
+                }}
+            />
+        </Tab.Navigator>
+    );
+};
+
+export default BottomTabNavigator;
